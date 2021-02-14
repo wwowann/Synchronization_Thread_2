9d1a605c686aee7d3ac2c9de5045b011c5cfb525
”wwowann” <ivasin75@mail.ru>
2021-02-14 16:50:03 +0000

    add all

--- src/main/java/CarsSalon.java
+++ src/main/java/CarsSalon.java
@@ -1,62 +1,51 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class CarsSalon {
     private final List<Car> cars = new ArrayList<>();
     private final ReentrantLock lock = new ReentrantLock();
-    private final ReentrantLock lock1 = new ReentrantLock();
-    private final ReentrantLock lock2 = new ReentrantLock();
-    final Object object1 = new Object();
-
+    private final Condition condition = lock.newCondition();
 
     public void sellCar() {
         int count = 0;
         while (count < 4) {
-            System.out.println(Thread.currentThread().getName() + " покупает");
-            lock1.lock();
-            synchronized (object1) {
+            try {
+                lock.lock();
+                System.out.println(Thread.currentThread().getName() + " покупает");
                 if (cars.size() == 0) {
                     System.out.println("Автомобилей нет на складе, подождите привоза новых автомобилей");
+                    condition.await();
                 }
-                try {
-                    object1.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                object1.notify();
-            }
+                System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
 
-            System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
-
-            cars.remove(0);
-            lock1.unlock();
-            System.out.println("Осталось " + cars.size() + " автомобилей");
-            try {
-                Thread.sleep(1000);
-
+                System.out.println("Осталось " + cars.size() + " автомобилей");
+                count++;
             } catch (InterruptedException e) {
                 e.printStackTrace();
+            } finally {
+                lock.unlock();
             }
-            count++;
+            cars.remove(0);
         }
     }
 
-
     public void acceptCar() {
         int count = 0;
-        try {
-            while (count < 12) {
+        while (count < 12) {
+            try {
+                lock.lock();
                 System.out.println("Производитель выпустил новый автомобиль. Можно продавать");
-                synchronized (object1) {
-                    cars.add(new Car());
-                    object1.notify();
-                }
+                cars.add(new Car());
+                condition.signal();
                 Thread.sleep(2000);
                 count++;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
         System.out.println("Производство автомобилей закончено, кина не будет");
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,20 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayList;
+
+import java.util.List;
+
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         CarsSalon carsSalon = new CarsSalon();
-//        Thread threadProducedCar = new Thread();
-        new Thread(null, carsSalon::acceptCar, "Продавец ").start();
-        startThread(carsSalon);
-    }
 
-    public static void startThread(CarsSalon carsSalon) {
-        new Thread(null, carsSalon::sellCar, "Покупатель 1 ").start();
-        new Thread(null, carsSalon::sellCar, "Покупатель 2 ").start();
-        new Thread(null, carsSalon::sellCar, "Покупатель 3 ").start();
+        List<Thread> threadList = new ArrayList<>();
+        threadList.add(new Thread(null, carsSalon::sellCar, "Покупатель 1 "));
+        threadList.add(new Thread(null, carsSalon::sellCar, "Покупатель 2 "));
+        threadList.add(new Thread(null, carsSalon::sellCar, "Покупатель 3 "));
+        for (Thread thread : threadList) {
+            thread.start();
+        }
+        new Thread(null, carsSalon::acceptCar, "Продавец ").start();
     }
 }
